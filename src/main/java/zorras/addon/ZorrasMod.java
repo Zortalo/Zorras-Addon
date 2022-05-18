29c98a39c5c44d6bffdb9b3db831c4ae9aaa2b8d
Zorra <rexzob35@gmail.com>
2022-05-18 13:59:36 -0700

    Removed All Items and Textures. Remapped power registry to ZorrasPowerFactory

--- src/main/java/zorras/addon/ZorrasMod.java
+++ src/main/java/zorras/addon/ZorrasMod.java
@@ -4,36 +4,25 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.loader.api.FabricLoader;
-import zorras.addon.item.*;
+import net.minecraft.util.Identifier;
 import zorras.addon.registry.*;
 
 public class ZorrasMod implements ModInitializer {
 	public static final String MOD_ID = "zorras";
 	public static final Logger LOGGER = LogManager.getLogger(ZorrasMod.class);
 	public static String VERSION = "";
-//Origins Nonsense
+
+	public static Identifier identifier(String path) {
+		return new Identifier(MOD_ID, path);
+	}
+
 
 	@Override
 	public void onInitialize() {
-		FabricLoader.getInstance().getModContainer(MOD_ID).ifPresent(modContainer -> {
-			VERSION = modContainer.getMetadata().getVersion().getFriendlyString();
-			if(VERSION.contains("+")) {
-				VERSION = VERSION.split("\\+")[0];
-			}
-			if(VERSION.contains("-")) {
-				VERSION = VERSION.split("-")[0];
-			}
-		});
-		LOGGER.info("Zorras Addon " + VERSION + " is active. Enjoy the show!");
+		//Log
+		LOGGER.info("Zorras Addon is active. Enjoy the show!");
+
 		//Origin Registry
-		ModPowers.init();
-		//Item Registery
-		WyrmString.registerItems();
-      DinoNugget.registerItems();
-		GoldenDinoNugget.registerItems();
-	}
-	public static void init() {
-		
+		ZorrasPowerFactory.register();
 	}
 }
---
+++ src/main/java/zorras/addon/registry/ZorrasPowerFactory.java
@@ -0,0 +1,18 @@
+package zorras.addon.registry;
+
+import io.github.apace100.apoli.power.factory.PowerFactory;
+import io.github.apace100.apoli.registry.ApoliRegistries;
+import net.minecraft.util.registry.Registry;
+import zorras.addon.power.*;
+
+public class ZorrasPowerFactory {
+
+   public static void register(){
+      register(PowderSnowSink.getFactory());
+  }
+
+
+public static void register(PowerFactory<?> serializer) {
+   Registry.register(ApoliRegistries.POWER_FACTORY, serializer.getSerializerId(), serializer);
+}
+}
